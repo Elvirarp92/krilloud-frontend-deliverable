3d8a30f5bf416113968db0daf0a10093cc407ab8
Elvira Ramírez <elvirarp92@gmail.com>
2020-12-14 18:07:27 +0100

    Add animation to card flip
    There is still a minor issue - the content changes before the flip
    happens. To be corrected in a later iteration.

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,14 @@
 import { useState } from 'react'
+import './Card.css'
 
 const Card = (props) => {
   const [isFlipped, setIsFlipped] = useState(false)
 
   return (
-    <div onMouseEnter={() => setIsFlipped(true)} onMouseLeave={() => setIsFlipped(false)}>
+    <div
+      onMouseEnter={() => setIsFlipped(true)}
+      onMouseLeave={() => setIsFlipped(false)}
+      class={`card ${isFlipped ? 'isActive' : ''}`}>
       {!isFlipped && props.children[0]}
       {isFlipped && props.children[1]}
     </div>
